3c256c75b5bb99e4c30f304083153ed590c3a266
Alinur01 <Alinursatylganov8@gmail.com>
2023-11-03 11:55:19 +0600

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import logo from './media/logo.jpg';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="centered-block">
+    <h1>ALA-TOO</h1>
+    <img className='logo' src={logo} alt="Логотип" />
+      <App />
+    </div>
   </React.StrictMode>
 );
